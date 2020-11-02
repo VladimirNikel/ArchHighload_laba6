1da9415757970164827a752569cf3cbe8d0bf6e2
VladimirNikel <vova_niemisto@mail.ru>
2020-11-02 15:36:20 +0300

    Добавлен функционал сбора данных

--- check.py
+++ check.py
@@ -7,7 +7,7 @@ import sys
 
 def collection_metrics(server_path: str, login:str, password: str):
 	url='http://'+server_path+'/metrics'
-	print(url)
+	#print(url)
 	current_date_utc= datetime.datetime.utcnow().strftime("%Y.%m.%d_%H:%M:%S")
 	input_value		= login+current_date_utc+password
 	calculate_hash	= str(hashlib.sha256(input_value.encode()).hexdigest())
@@ -16,13 +16,13 @@ def collection_metrics(server_path: str, login:str, password: str):
 	    ('hash_sum', calculate_hash)
 	)
 	response = requests.get(url, params=params)
-	return json.loads(response.content.decode())
+	return json.dumps({"server_path": server_path, "date_utc": current_date_utc, "data": json.loads(response.content.decode())})
 
-if len(sys.argv) == 3:
-	login		= str(sys.argv[1])
-	password	= str(sys.argv[2])
-else:
-	login		= str(input("2Введите логин: "))
-	password	= str(getpass.getpass("2Введите пароль: "))
+#if len(sys.argv) == 3:
+#	login		= str(sys.argv[1])
+#	password	= str(sys.argv[2])
+#else:
+#	login		= str(input("Введите логин: "))
+#	password	= str(getpass.getpass("Введите пароль: "))
 
-print(collection_metrics("127.0.0.1:8000", login, password))
+#print(json.loads(collection_metrics("127.0.0.1:8000", login, password)))
--- monitor.py
+++ monitor.py
@@ -1,16 +1,30 @@
 from check import collection_metrics
 import getpass
 import datetime
+import threading
+import json
 
-login		= str(input("1Введите логин: "))
-password	= str(getpass.getpass("1Введите пароль: "))
+login		= str(input("Введите логин: "))
+password	= str(getpass.getpass("Введите пароль: "))
+time_to_wait= int(input("Введите количество секунд, через которое будут опрашиваться сервера: "))
+if time_to_wait < 1:
+	time_to_wait=60
+	print("Вы ввели некорректное значение. Выставлены стандартные настройки: опрос серверов будет происходить каждые 60 секунд.")
 
-pull_serv=["127.0.0.1:8000","194.61.2.84:5020", "194.61.2.84:5030"]
+pull_serv=["127.0.0.1:8000", "194.61.2.84:5020", "194.61.2.84:5030"]
+print("Начал работать в", datetime.datetime.now().strftime("%H:%M:%S %d.%m.%Y"))
 
-for i in range(len(pull_serv)):
-	print("я потопал в ",pull_serv[i])
-	print(collection_metrics(pull_serv[i], login, password))
+def main_job():
+	for i in range(len(pull_serv)):
+		print(json.loads(collection_metrics(pull_serv[i], login, password)))
 
+try:
+	while 1:
+		print("Данные на", datetime.datetime.now().strftime("%H:%M:%S %d.%m.%Y"))
+		t = threading.Timer(time_to_wait, main_job)
+		t.start()
+		t.join()
 
-
-
+except KeyboardInterrupt:
+	print("\n\nЧто ж, до встречи...\nОсталось долждаться ответа от количества потоков: ",threading.active_count()-1)
+	exit(0)
